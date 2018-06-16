80ae66728586955ee94633fddddfab68c6722599
miztli <dark_coneja@hotmail.com>
2018-06-16 15:21:04 -0500

    Add logback configuration

--- src/main/groovy/com/samples/propertiessamples/PropertiesSamplesApplication.groovy
+++ src/main/groovy/com/samples/propertiessamples/PropertiesSamplesApplication.groovy
@@ -1,12 +1,17 @@
 package com.samples.propertiessamples
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
 class PropertiesSamplesApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesSamplesApplication.class);
+
 	static void main(String[] args) {
+		LOGGER.info("---Booting PropertiesSamplesApplication---")
 		SpringApplication.run PropertiesSamplesApplication, args
 	}
 }
